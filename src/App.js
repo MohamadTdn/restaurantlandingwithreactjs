81de8d93403a82742bf592df7fb3974086bf1cf9
MohamadTdn <mohamadmt1579@gmail.com>
2024-05-17 23:44:32 +0430

    create about component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header'
+import About from './Components/About/About'
 
 const App = () => {
   return (
     <div className="App">
       <Header></Header>
+      <About></About>
     </div>
   );
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,17 @@
+import './About.css'
+
+const About = () => {
+    return (
+        <div className='About-section'>
+            <div>
+            <h1>Welcome</h1>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</p>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth</p>
+            <button className='Learn-more-btn'>Learn More About Us</button>
+            </div>
+            <img src="assets/about.webp" alt="" />
+       </div>
+    )
+}
+
+export default About
